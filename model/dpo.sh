a3a61a8fff75c4d837c77b79388bd67e0d9127eb
LucaS <luca.engel@epfl.ch>
2025-09-19 03:48:37 +0200

    add files to git

---
+++ model/dpo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH --chdir /scratch/izar/engel/MNLP_M2/model/
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 85G
+#SBATCH --time 11:30:00
+#SBATCH --gres gpu:1
+#SBATCH --account cs-552
+#SBATCH --qos cs-552
+
+date
+module load gcc python
+
+# virtualenv --system-site-packages ~/venvs/mnlp_m2
+
+source ~/venvs/mnlp_m2/bin/activate
+
+pip install --upgrade pip
+pip3 install torch==2.1.0 --index-url https://download.pytorch.org/whl/cu118
+
+pip3 install -r ./requirements.txt
+
+# pip3 install --upgrade deepspeed
+
+
+
+echo "Install Dependencies complete"
+echo "Running the code"
+
+python3 ./dpo_script.py
+
+
+echo "Run completed on $(hostname)"
+sleep 2
+date
